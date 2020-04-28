4c408f4c96011024032e7df7c0298c503bd0d3d6
Eder Rodriguez <ederrod21@gmail.com>
2020-04-28 15:34:42 -0600

    checking if necessary packages are installed

--- pick.sh
+++ pick.sh
@@ -2,8 +2,27 @@
 # TODO write script so that it checks if splunk is installed in device
 # and also mongodb
 
-sudo su - splunk -c "cd bin; ./splunk start"
+if dpkg-query --list | grep -i splunk > /dev/null
+then 
+    echo "Starting Splunk"
+    sudo su - splunk -c "cd bin; ./splunk start"
+else   
+    # Install
+    echo "Splunk was not found in this system. Installing Splunk..."
+    # wget -O splunk-8.0.3-a6754d8441bf-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.3&product=splunk&filename=splunk-8.0.3-a6754d8441bf-linux-2.6-amd64.deb&wget=true'
+    # dpkg -i splunk-8.0.3-a6754d8441bf-linux-2.6-amd64.deb
 
-sudo systemctl start mongod
+    # echo "Splunk installed... Starting Splunk"
+    # sudo su - splunk -c "cd bin; ./splunk start"
+fi
+
+if which mongod > /dev/null
+then
+    echo "Mongodb detected starting mongo..."
+    sudo systemctl start mongod
+else
+    echo "Mongodb not found. Installing mongo"
+    # TODO
+fi
 
 python3 src/main.py
