7ef4223e1398ad88533c2f1f731270d45299ab2d
Jose Gallardo <jagallardo6@miners.utep.edu>
2020-05-03 16:26:10 -0600

    Adding Combobox to table view

--- src/app/views/analysisview.py
+++ src/app/views/analysisview.py
@@ -1,14 +1,16 @@
 import sys
 sys.path.append("../..")
 from managers.vectormanager import VectorManager
+from managers.logentrymanager import LogEntry
 
 from app.views.graph.graphwidget import GraphWidget
 
-from PyQt5.QtCore import Qt, QSize
-from PyQt5.QtGui import QIcon, QPixmap, QStandardItemModel, QStandardItem
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import Qt, QSize, QEvent
+from PyQt5.QtGui import QIcon, QPixmap, QStandardItemModel, QStandardItem, QFontMetrics, QPalette
 from PyQt5.QtWidgets import QWidget, QDialog,QFrame, QGridLayout, QHBoxLayout, QVBoxLayout, QTableView,QTableWidget, QTabWidget,\
                             QListWidget, QListWidgetItem, QLineEdit, QComboBox, QSpacerItem, QSizePolicy, QAction, QAbstractItemView,\
-                            QHeaderView
+                            QHeaderView, QListWidget, QStyledItemDelegate, qApp
 
 class AnalysisView(QWidget): 
     def __init__(self, parent=None): 
@@ -19,12 +21,13 @@ class AnalysisView(QWidget):
     def initUI(self): 
         self.setupMainLayout()
         self.setLayout(self.mainLayout)
+        self.testrow()
 
     def setupMainLayout(self): 
         #Log Entries table
         self.logEntriesTbl = QTableView()
         self.logEntryModel = QStandardItemModel()
-        self.logEntryModel.setHorizontalHeaderLabels(['Host', 'Timestamp', 'Content', 'Source', 'Source Type'])
+        self.logEntryModel.setHorizontalHeaderLabels(['Host', 'Timestamp', 'Content', 'Source', 'Source Type', 'Associated Vectors'])
         self.logEntriesTbl.setModel(self.logEntryModel)
         self.logEntriesTbl.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
@@ -41,14 +44,14 @@ class AnalysisView(QWidget):
         vectorLbl.setFlags(Qt.NoItemFlags)
 
         # Defined Vectors list
-        self.vectorWidget = QListWidget()
-        self.vectorWidget.setDragDropMode(QAbstractItemView.DragDrop)
-        self.vectorWidget.setAcceptDrops(True)
-        self.vectorWidget.addItem(vectorLbl)
-        self.vectorWidget.itemActivated.connect(self.setVectorSelected)
+        #self.vectorWidget = QListWidget()
+        #self.vectorWidget.setDragDropMode(QAbstractItemView.DragDrop)
+        #self.vectorWidget.setAcceptDrops(True)
+        #self.vectorWidget.addItem(vectorLbl)
+        #self.vectorWidget.itemActivated.connect(self.setVectorSelected)
 
         self.workspace = QHBoxLayout()
-        self.workspace.addWidget(self.vectorWidget, 10)
+        #self.workspace.addWidget(self.vectorWidget, 10)
         self.workspace.addWidget(self.tabWidget,90)
 
         # Filtering and search
@@ -104,7 +107,7 @@ class AnalysisView(QWidget):
             item.setText(vector.getName()) 
             item.setIcon(icon)
             item.setSizeHint(QSize(0, 50))
-            self.vectorWidget.addItem(item)
+            #self.vectorWidget.addItem(item)
 
     def addLogEntry(self, logentry):
         host = QStandardItem(logentry.getHost())
@@ -113,16 +116,149 @@ class AnalysisView(QWidget):
         source = QStandardItem(logentry.getSource())
         sourcetype = QStandardItem(logentry.getSourceType())
 
+        testWidget = QtWidgets.QWidget()
+        combobox = CheckableComboBox(testWidget)
+        #combobox.setModel(self.logEntryModel)
+        vectors = self.vectorManager.getVectors()
+        for vector in vectors: 
+            combobox.addItem(vector.name)
+
         self.logEntryModel.appendRow([
             host,
             timestamp, 
             content,
             source,
-            sourcetype
-        ])
-        self.logEntriesTbl.setModel(self.logEntryModel)
+            sourcetype,
+        ]) 
+
+        row = self.logEntryModel.rowCount() - 1
+        col = self.logEntryModel.columnCount() - 1
+        a = self.logEntryModel.index(row, col)
+        self.logEntriesTbl.setIndexWidget(a, combobox)
 
     def setVectorSelected(self, item): 
         selVecName = item.text()
         self.vectorManager.setCurrentVector(selVecName)
-    
+
+    def testrow(self):
+        test_logEntry = LogEntry("host", "timestamp", "content", "source", "sourceType")
+        self.addLogEntry(test_logEntry)
+
+
+class CheckableComboBox(QComboBox):
+
+    # Subclass Delegate to increase item height
+    class Delegate(QStyledItemDelegate):
+        def sizeHint(self, option, index):
+            size = super().sizeHint(option, index)
+            size.setHeight(20)
+            return size
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Make the combo editable to set a custom text, but readonly
+        self.setEditable(True)
+        self.lineEdit().setReadOnly(True)
+        # Make the lineedit the same color as QPushButton
+        palette = qApp.palette()
+        palette.setBrush(QPalette.Base, palette.button())
+        self.lineEdit().setPalette(palette)
+
+        # Use custom delegate
+        self.setItemDelegate(CheckableComboBox.Delegate())
+
+        # Update the text when an item is toggled
+        self.model().dataChanged.connect(self.updateText)
+
+        # Hide and show popup when clicking the line edit
+        self.lineEdit().installEventFilter(self)
+        self.closeOnLineEditClick = False
+
+        # Prevent popup from closing when clicking on an item
+        self.view().viewport().installEventFilter(self)
+
+    def resizeEvent(self, event):
+        # Recompute text to elide as needed
+        self.updateText()
+        super().resizeEvent(event)
+
+    def eventFilter(self, object, event):
+
+        if object == self.lineEdit():
+            if event.type() == QEvent.MouseButtonRelease:
+                if self.closeOnLineEditClick:
+                    self.hidePopup()
+                else:
+                    self.showPopup()
+                return True
+            return False
+
+        if object == self.view().viewport():
+            if event.type() == QEvent.MouseButtonRelease:
+                index = self.view().indexAt(event.pos())
+                item = self.model().item(index.row())
+
+                if item.checkState() == Qt.Checked:
+                    item.setCheckState(Qt.Unchecked)
+                else:
+                    item.setCheckState(Qt.Checked)
+                return True
+        return False
+
+    def showPopup(self):
+        super().showPopup()
+        # When the popup is displayed, a click on the lineedit should close it
+        self.closeOnLineEditClick = True
+
+    def hidePopup(self):
+        super().hidePopup()
+        # Used to prevent immediate reopening when clicking on the lineEdit
+        self.startTimer(100)
+        # Refresh the display text when closing
+        self.updateText()
+
+    def timerEvent(self, event):
+        # After timeout, kill timer, and reenable click on line edit
+        self.killTimer(event.timerId())
+        self.closeOnLineEditClick = False
+
+    def updateText(self):
+        texts = []
+        for i in range(self.model().rowCount()):
+            if self.model().item(i).checkState() == Qt.Checked:
+                texts.append(self.model().item(i).text())
+        text = ", ".join(texts)
+
+        # Compute elided text (with "...")
+        metrics = QFontMetrics(self.lineEdit().font())
+        elidedText = metrics.elidedText(text, Qt.ElideRight, self.lineEdit().width())
+        self.lineEdit().setText(elidedText)
+
+    def addItem(self, text, data=None):
+        item = QStandardItem()
+        item.setText(text)
+        if data is None:
+            item.setData(text)
+        else:
+            item.setData(data)
+        item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsUserCheckable)
+        item.setData(Qt.Unchecked, Qt.CheckStateRole)
+        self.model().appendRow(item)
+
+    def addItems(self, texts, datalist=None):
+        for i, text in enumerate(texts):
+            try:
+                data = datalist[i]
+            except (TypeError, IndexError):
+                data = None
+            self.addItem(text, data)
+
+    def currentData(self):
+        # Return the list of selected items data
+        res = []
+        for i in range(self.model().rowCount()):
+            if self.model().item(i).checkState() == Qt.Checked:
+                res.append(self.model().item(i).data())
+        return res
+   
